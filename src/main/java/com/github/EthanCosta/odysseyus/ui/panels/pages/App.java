3ee7c0afb9a4f39cab6556d0ad624f48cce7a814
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 18:31:14 +0200

    Side Menu + user 's information

--- src/main/java/com/github/EthanCosta/odysseyus/ui/panels/pages/App.java
+++ src/main/java/com/github/EthanCosta/odysseyus/ui/panels/pages/App.java
@@ -11,6 +11,8 @@ import javafx.geometry.HPos;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
@@ -105,6 +107,60 @@ public class App extends Panel {
         settingsBtn.setOnMouseClicked(e -> setPage(null, settingsBtn));
 
         sidemenu.getChildren().addAll(homeBtn, settingsBtn);
+
+        // Pseudo + avatar
+        GridPane userPane = new GridPane();
+        setCanTakeAllWidth(userPane);
+        userPane.setMaxHeight(80);
+        userPane.setMinWidth(80);
+        userPane.getStyleClass().add("user-pane");
+        setBottom(userPane);
+
+        String avatarUrl = "https://crafatar.com/renders/head/" + (
+                saver.get("offline-username") != null ?
+                        "MHF_Steve.png" :
+                        Launcher.getInstance().getAuthInfos().getUuid() + ".png"
+        );
+        ImageView avatarView = new ImageView();
+        Image avatarImg = new Image(avatarUrl);
+        avatarView.setImage(avatarImg);
+        avatarView.setPreserveRatio(true);
+        avatarView.setFitHeight(50d);
+        setCenterV(avatarView);
+        setCanTakeAllSize(avatarView);
+        setLeft(avatarView);
+        avatarView.setTranslateX(15d);
+        userPane.getChildren().add(avatarView);
+
+        Label usernameLabel = new Label(Launcher.getInstance().getAuthInfos().getUsername());
+        usernameLabel.setFont(Font.font("Consolas", FontWeight.BOLD, FontPosture.REGULAR, 25f));
+        setCanTakeAllSize(usernameLabel);
+        setCenterV(usernameLabel);
+        setLeft(usernameLabel);
+        usernameLabel.getStyleClass().add("username-label");
+        usernameLabel.setTranslateX(75d);
+        setCanTakeAllWidth(usernameLabel);
+        userPane.getChildren().add(usernameLabel);
+
+        Button logoutBtn = new Button();
+        FontAwesomeIconView logoutIcon = new FontAwesomeIconView(FontAwesomeIcon.SIGN_OUT);
+        logoutIcon.getStyleClass().add("logout-icon");
+        setCanTakeAllSize(logoutBtn);
+        setCenterV(logoutBtn);
+        setRight(logoutBtn);
+        logoutBtn.getStyleClass().add("logout-btn");
+        logoutBtn.setGraphic(logoutIcon);
+        logoutBtn.setOnMouseClicked(e -> {
+            saver.remove("accessToken");
+            saver.remove("clientToken");
+            saver.remove("offline-username");
+            saver.save();
+            Launcher.getInstance().setAuthInfos(null);
+            this.panelManager.showPanel(new Login());
+        });
+        userPane.getChildren().add(logoutBtn);
+        sidemenu.getChildren().add(userPane);
+
 
 
 
