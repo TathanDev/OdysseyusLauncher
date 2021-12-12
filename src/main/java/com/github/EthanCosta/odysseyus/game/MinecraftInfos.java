a7b8a5125d57bb767476ff26365d63367c5e1b22
TATHAN <unconfigured@null.spigotmc.org>
2021-12-12 14:02:45 +0100

    Forge 1.16.5-36.2.20

--- src/main/java/com/github/EthanCosta/odysseyus/game/MinecraftInfos.java
+++ src/main/java/com/github/EthanCosta/odysseyus/game/MinecraftInfos.java
@@ -10,7 +10,7 @@ public class MinecraftInfos {
     public static final String GAME_VERSION = "1.16.5";
     public static final VersionType VERSION_TYPE = VersionType.FORGE;
     public static final ForgeVersionBuilder.ForgeVersionType FORGE_VERSION_TYPE = ForgeVersionBuilder.ForgeVersionType.NEW;
-    public static final String FORGE_VERSION = "1.16.5-36.2.4";
+    public static final String FORGE_VERSION = "1.16.5-36.2.20";
     public static final String OPTIFINE_VERSION = "1.16.5_HD_U_G8";
 
     public static final GameType OLL_GAME_TYPE = GameType.V1_13_HIGHER_FORGE;
