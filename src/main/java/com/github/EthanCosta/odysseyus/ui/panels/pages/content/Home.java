6586823a0a81b805f1de5b6d5e573599be4c135d
TATHAN <67022946+ethancosta@users.noreply.github.com>
2021-12-18 17:35:59 +0100

    ✅ Add My server is Compatible
    Signed-off-by: TATHAN <unconfigured@null.spigotmc.org>

--- src/main/java/com/github/EthanCosta/odysseyus/ui/panels/pages/content/Home.java
+++ src/main/java/com/github/EthanCosta/odysseyus/ui/panels/pages/content/Home.java
@@ -182,6 +182,8 @@ public class Home extends contentpanel {
             modInfos.add(new CurseFileInfo(377448, 3549966)); //Space Bosstools
             modInfos.add(new CurseFileInfo(546554, 3550030)); //Space Bosstools Addons
             //modInfos.add(new CurseFileInfo(284324, 3543338)); //Emojiful ( A rajouter sur Odysseyus)
+            modInfos.add(new CurseFileInfo(492574, 3449703)); //My server is Compatible
+
 
 
             modInfos.addAll(addons.modAddons);
