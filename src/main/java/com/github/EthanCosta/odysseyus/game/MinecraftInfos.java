6036d9ec45da9beb3a6f3942c44cf9db8c1dfc52
TATHAN <unconfigured@null.spigotmc.org>
2021-12-18 16:30:31 +0100

    💿 Change a link
    Signed-off-by: TATHAN <unconfigured@null.spigotmc.org>

--- src/main/java/com/github/EthanCosta/odysseyus/game/MinecraftInfos.java
+++ src/main/java/com/github/EthanCosta/odysseyus/game/MinecraftInfos.java
@@ -11,7 +11,6 @@ public class MinecraftInfos {
     public static final ForgeVersionBuilder.ForgeVersionType FORGE_VERSION_TYPE = ForgeVersionBuilder.ForgeVersionType.NEW;
     public static final String FORGE_VERSION = "1.16.5-36.2.20";
     public static final String OPTIFINE_VERSION = "1.16.5_HD_U_G8";
-
-    public static final String MODS_LIST_URL = "https://odysseyus.fr/mods-test.json";
+    public static final String MODS_LIST_URL = "https://odysseyus.fr/mods.json";
 
 }
